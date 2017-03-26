38269b69e112ab9e2ff2d7c641149a24f98fc5fb
danhunter <d4nhunter@gmail.com>
2017-03-26 21:45:31 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+var Alexa = require("alexa-sdk");
+var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = timesTables;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+
+/** TODO - Read Times Tables
+* Create times tables set 2 - 12.
+* Ask: which times tables would you like. 2 - 12
+* Tell: Read times tables set
+* end
+* help
+* Stop
+* Cancel
+* help
+**/
+
+
+/** TODO - Times Tables Quiz **/
