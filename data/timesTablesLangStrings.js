7f321af4ef4ad267c1b35e75da8cf547837e1570
Dan Hunter <danhu@motabilityoperations.co.uk>
2017-03-31 16:57:05 +0100

    implemented get times table function

--- data/timesTablesLangStrings.js
+++ data/timesTablesLangStrings.js
@@ -1,21 +1,35 @@
 
+var delay = "1s";
+
+var timesTablesLangStrings =
 {
   "en": {
-      "translation": {
-          "TIMES_TABLES": [
-              "",
-              ""
-          ],
-          "SKILL_NAME" : "Times Tables",
-          "TIMES_TABLES_MESSAGE" : "Here's your table: ",
-          "HELP_MESSAGE" : "You can say read times 2 tables, up to the 12 times tables or, you can say exit... What can I help you with?",
-          "HELP_REPROMPT" : "What can I help you with?",
-          "STOP_MESSAGE" : "Goodbye!"
-      }
-  },
-  "en-US": {
-
+    "translation": {
+        "SKILL_NAME" : "Times Tables",
+        "TIMES_TABLES_MESSAGE" : "Here's your times table: ",
+        "HELP_MESSAGE" : "I will read out times tables between 2 and 12. You can say read 2 times table, or you can say exit... What can I help you with?",
+        "HELP_REPROMPT" : "What can I help you with?",
+        "STOP_MESSAGE" : "Goodbye!"
+    }
   },
   "en-GB": {
-
+    "translation": {
+        "SKILL_NAME" : "Times Tables",
+        "TIMES_TABLES_MESSAGE" : "Here's your times table: ",
+        "HELP_MESSAGE" : "I will read out times tables between 2 and 12. You can say read 2 times table, or you can say exit... What can I help you with?",
+        "HELP_REPROMPT" : "What can I help you with?",
+        "STOP_MESSAGE" : "Goodbye!"
+    }
+  },
+  "en-US": {
+    "translation": {
+        "SKILL_NAME" : "Times Tables",
+        "TIMES_TABLES_MESSAGE" : "Here's your times table: ",
+        "HELP_MESSAGE" : "I will read out times tables between 2 and 12. You can say read 2 times table, or you can say exit... What can I help you with?",
+        "HELP_REPROMPT" : "What can I help you with?",
+        "STOP_MESSAGE" : "Goodbye!"
+    }
+  }
 };
+
+exports.timesTablesLangStrings = timesTablesLangStrings;
--- index.js
+++ index.js
@@ -4,28 +4,54 @@
 var Alexa = require("alexa-sdk");
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
-var timesTablesLangStrings;
+var ttModule = require('./data/timesTablesLangStrings.js');
+var ttLangStrings = ttModule.timesTablesLangStrings;
+
+var delay = "1s";
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = timesTablesLangStrings;
+    alexa.resources = ttLangStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
+var handlers = {
+  'LaunchRequest': function () {
+      this.emit('GetTimesTable');
+  },
+  'TimesTablesIntent': function () {
+      this.emit('GetTimesTable');
+  },
+  'GetTimesTable': function () {
+      var timesTableNumber = parseInt(this.event.request.intent.slots.number.value);
+      console.log('Times tables selected: ' + timesTableNumber);
 
-/** TODO - Read Times Tables
-* Create times tables set 2 - 12.
-* Ask: which times tables would you like. 2 - 12
-* Tell: Read times tables set
-* end
-* help
-* Stop
-* Cancel
-* help
-**/
+      for (var i = 0; i <= 12; i++) {
+        var result = i * timesTableNumber;
+        this.emit(':tell', i + " times " + timesTableNumber + " equals <break time=\"${delay}\" />" + result);
+      }
+
+      // var ttArray = this.t('TIMES_TABLES');
+      // var timesTable = ttArray.timesTableNumber;
+      // var speechOutput = this.t("TIMES_TABLES_MESSAGE") + timesTable;
+      // this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), timesTable)
+  },
+  'AMAZON.HelpIntent': function () {
+      var speechOutput = this.t("HELP_MESSAGE");
+      var reprompt = this.t("HELP_MESSAGE");
+      this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function () {
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+  },
+  'AMAZON.StopIntent': function () {
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+  }
+
+  /** TODO - Times Tables Quiz **/
 
 
-/** TODO - Times Tables Quiz **/
+};
