17a6eba949b5628104c0c4ec2eedad9de49f7bb0
Dan Hunter <danhu@motabilityoperations.co.uk>
2017-04-05 16:33:35 +0100

    Building test mode

--- data/timesTablesLangStrings.js
+++ data/timesTablesLangStrings.js
@@ -4,7 +4,10 @@ var timesTablesLangStrings =
   "en": {
     "translation": {
         "SKILL_NAME" : "Times Tables",
-        "TIMES_TABLES_MESSAGE" : "Here's your times table: ",
+        "TIMES" : " times ",
+        "IS" : " is ",
+        "TIMES_TABLES_MESSAGE" : "Here's your times table ",
+        "NEXT_TIME" : "OK, see you next time!",
         "HELP_MESSAGE" : "I will read out times tables between 2 and 12. You can say read 2 times table, or you can say exit... What can I help you with?",
         "HELP_REPROMPT" : "What can I help you with?",
         "STOP_MESSAGE" : "Goodbye!"
@@ -13,7 +16,10 @@ var timesTablesLangStrings =
   "en-GB": {
     "translation": {
         "SKILL_NAME" : "Times Tables",
-        "TIMES_TABLES_MESSAGE" : "Here's your times table: ",
+        "TIMES" : " times ",
+        "IS" : " is ",
+        "TIMES_TABLES_MESSAGE" : "Here's your times table ",
+        "NEXT_TIME" : "OK, see you next time!",
         "HELP_MESSAGE" : "I will read out times tables between 2 and 12. You can say read 2 times table, or you can say exit... What can I help you with?",
         "HELP_REPROMPT" : "What can I help you with?",
         "STOP_MESSAGE" : "Goodbye!"
@@ -22,10 +28,37 @@ var timesTablesLangStrings =
   "en-US": {
     "translation": {
         "SKILL_NAME" : "Times Tables",
-        "TIMES_TABLES_MESSAGE" : "Here's your times table: ",
+        "TIMES" : " times ",
+        "IS" : " is ",
+        "TIMES_TABLES_MESSAGE" : "Here's your times table ",
+        "NEXT_TIME" : "OK, see you next time!",
         "HELP_MESSAGE" : "I will read out times tables between 2 and 12. You can say read 2 times table, or you can say exit... What can I help you with?",
         "HELP_REPROMPT" : "What can I help you with?",
         "STOP_MESSAGE" : "Goodbye!"
+    }
+  },
+  "de": {
+    "translation": {
+        "SKILL_NAME" : "Mal Tisch",
+        "TIMES" : " mal ",
+        "IS" : " ist ",
+        "TIMES_TABLES_MESSAGE" : "Hier ist dein mal tisch ",
+        "NEXT_TIME" : "Okay, bis zum nächsten Mal!",
+        "HELP_MESSAGE" : "Ich lese mal Tische zwischen 2 und 12 aus. Man kann sagen, 2 mal Tisch zu lesen, oder man kann sagen, Ausfahrt ... Was kann ich Ihnen helfen?",
+        "HELP_REPROMPT" : "Womit kann ich dir helfen?",
+        "STOP_MESSAGE" : "Auf Wiedersehen!"
+    }
+  },
+  "fr": {
+    "translation": {
+        "SKILL_NAME" : "Tableaux de temps",
+        "TIMES" : " fois ",
+        "IS" : " est ",
+        "TIMES_TABLES_MESSAGE" : "Voici votre table des temps ",
+        "NEXT_TIME" : "OK, à la prochaine!",
+        "HELP_MESSAGE" : "Je vais lire les tableaux des temps entre 2 et 12. Vous pouvez dire lire 2 fois le tableau, ou vous pouvez dire la sortie ... À quoi puis-je vous aider?",
+        "HELP_REPROMPT" : "À quoi puis-je vous aider?",
+        "STOP_MESSAGE" : "Au Revoir!"
     }
   }
 };
--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 'use strict';
 
 var Alexa = require("alexa-sdk");
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
-
 var ttModule = require('./data/timesTablesLangStrings.js');
 var ttLangStrings = ttModule.timesTablesLangStrings;
 
-var delay = "0.3s";
+var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+var DELAY_SECS = "0.3s";
+var DELAY = "<break time='" + DELAY_SECS + "' /> ";
+var TEST_STARTING_NUMBER = 0;
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -18,6 +19,13 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
+/** TODO
+*  1) Add test option Ask: "Read out times tables or start a times tables test?" Currently just reads out choosen times tables.
+*  2) Look at ways to do TDD currently just live testing using skills website.
+*  3) Remove duplicate intent functions in handlers i.e. stop, cancel, etc.
+*  4) Move all speach into resources
+**/
+
 var handlers = {
   'LaunchRequest': function () {
       this.emit('GetTimesTable');
@@ -25,6 +33,7 @@ var handlers = {
   'TimesTablesIntent': function () {
       this.emit('GetTimesTable');
   },
+  /** Read out User choosen times tables **/
   'GetTimesTable': function () {
       var timesTableNumber = parseInt(this.event.request.intent.slots.number.value);
       console.log('Times tables selected: ' + timesTableNumber);
@@ -33,10 +42,10 @@ var handlers = {
       var i = 0;
       while (i < 13) {
         var result = i * timesTableNumber;
-        var resultString = i + " times " + timesTableNumber + " equals <break time='" + delay + "' /> " + result;
+        var resultString = i + this.t("TIMES") + timesTableNumber + this.t("IS") + DELAY + result;
 
         if (i < 12) {
-          timesTableResult = timesTableResult.concat(resultString + ", <break time='" + delay + "' />");
+          timesTableResult = timesTableResult.concat(resultString + ", " + DELAY);
         } else {
           timesTableResult = timesTableResult.concat(resultString);
         }
@@ -56,8 +65,122 @@ var handlers = {
   'AMAZON.StopIntent': function () {
       this.emit(':tell', this.t("STOP_MESSAGE"));
   }
-
-  /** TODO - Times Tables Quiz **/
-
-
 };
+
+  /** TIMES TABLES TEST
+  * Options:
+  * 1) Alexa asks 12 random times tables questions between sets 1-12, score tracked and final score given.
+  * 2) User selects to be tested on set between 1-12 (in order), if incorrect user can try question again or stop.
+  * 3) User selects to be tested on set between 1-12 (in order), score tracked and final score given, with incorrect answers.
+  *
+  * Proceeding with option 2 until more feedback recieved.
+  **/
+
+  var states = {
+      ANSWERMODE: '_ANSWERMODE', // User is trying to answer the times table question.
+      STARTMODE: '_STARTMODE'  // Prompt the user to start or restart the game.
+      DETERMINEMODE: "_DETERMINEMODE" // Determine which times tables the user want to be tested on
+  };
+
+  var newTestSessionHandlers = {
+    'NewSession': function() {
+      this.handler.state = states.STARTMODE;
+      this.emit(':ask', 'Welcome to the times tables test. Would you like to start?',
+                        'Say yes to start the test or no to quit.');
+    },
+    "AMAZON.StopIntent": function() {
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    "AMAZON.CancelIntent": function() {
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'SessionEndedRequest': function () {
+        console.log('session ended!');
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
+  }
+
+  var startTestHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
+    'NewSession': function () {
+        this.emit('NewSession'); // Uses the handler in newSessionHandlers
+    },
+    'AMAZON.HelpIntent': function() {
+        var message = 'I will read out a times tables questions, Do you want to start the test?';
+        this.emit(':ask', message, message);
+    },
+    'AMAZON.YesIntent': function() {
+        this.handler.state = states.DETERMINEMODE; // determine which times tables to be tested on
+        this.emit(':ask', 'Great! What times tables between 2 and 12 would you like to be tested on?.',
+                          'Say a number between 2 and 12.');
+    },
+    'AMAZON.NoIntent': function() {
+        console.log("NOINTENT");
+        this.emit(':tell', this.t("NEXT_TIME"));
+    },
+    "AMAZON.StopIntent": function() {
+      console.log("STOPINTENT");
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    "AMAZON.CancelIntent": function() {
+      console.log("CANCELINTENT");
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'SessionEndedRequest': function () {
+        console.log("SESSIONENDEDREQUEST");
+        //this.attributes['endedSessionCount'] += 1;
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'Unhandled': function() {
+        console.log("UNHANDLED");
+        var message = 'Say yes to continue, or no to end the test.';
+        this.emit(':ask', message, message);
+    }
+  });
+
+
+  var determineTestHandlers = Alexa.CreateStateHandler(states.DETERMINEMODE, {
+    'NewSession': function () {
+        this.emit('NewSession'); // Uses the handler in newSessionHandlers
+    },
+    'AMAZON.HelpIntent': function() {
+        var message = 'What times tables would you like to be tested on?.', 'Say a number between 2 and 12.';
+        this.emit(':ask', message, message);
+    },
+    'AMAZON.DetermineIntent': function() {
+        var timesTableNumber = parseInt(this.event.request.intent.slots.number.value);
+        this.attributes["timesTableNumber"] = timesTableNumber;
+        this.attributes["currentNumber"] = TEST_STARTING_NUMBER;
+
+        this.handler.state = states.ANSWERMODE;
+        this.emit(':ask', "What is " +  TEST_STARTING_NUMBER + this.t("TIMES") + timesTableNumber + "?", "Say a number.");
+    },
+    "AMAZON.StopIntent": function() {
+      console.log("STOPINTENT");
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    "AMAZON.CancelIntent": function() {
+      console.log("CANCELINTENT");
+      this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'SessionEndedRequest': function () {
+        console.log("SESSIONENDEDREQUEST");
+        //this.attributes['endedSessionCount'] += 1;
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    },
+    'Unhandled': function() {
+        console.log("UNHANDLED");
+        var message = 'Say yes to continue, or no to end the test.';
+        this.emit(':ask', message, message);
+    }
+  });
+
+  var answerModeHandlers = Alexa.CreateStateHandler(states.ANSWERMODE, {
+
+  });
+
+  var answerAttemptHandlers = {
+
+  };
+
+
+// end
